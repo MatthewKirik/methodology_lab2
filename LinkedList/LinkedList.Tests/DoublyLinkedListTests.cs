9fb22ad10e6fb1fc969e139f867444cf13742f7f
Matthew <ikirik59@gmail.com>
2022-06-08 15:20:26 +0300

    Add class for testing list

---
+++ LinkedList/LinkedList.Tests/DoublyLinkedListTests.cs
@@ -0,0 +1,12 @@
+﻿namespace LinkedList.Tests;
+
+public class DoublyLinkedListTests
+{
+    private readonly IDoublyLinkedList _sut;
+
+    public DoublyLinkedListTests()
+    {
+        _sut = new DoublyLinkedList();
+    }
+
+}
