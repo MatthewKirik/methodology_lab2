b52b2973ddfbfc41608105cdc69d9758fcf4d7cc
Matthew <ikirik59@gmail.com>
2022-06-08 15:17:45 +0300

    Add list implementation stub

---
+++ LinkedList/LinkedList/DoublyLinkedList.cs
@@ -0,0 +1,64 @@
+﻿namespace LinkedList;
+
+public class DoublyLinkedList : IDoublyLinkedList
+{
+    public int Length()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Append(char element)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Insert(char element, int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public char Delete(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void DeleteAll(char element)
+    {
+        throw new NotImplementedException();
+    }
+
+    public char Get(int index)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IDoublyLinkedList Clone()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Reverse()
+    {
+        throw new NotImplementedException();
+    }
+
+    public int FindFirst(char element)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int FindLast(char element)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Clear()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Extend(IDoublyLinkedList elements)
+    {
+        throw new NotImplementedException();
+    }
+}
