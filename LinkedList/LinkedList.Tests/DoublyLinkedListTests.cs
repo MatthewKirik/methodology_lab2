b25a3b308b977882e5af7ceda29990eb10c542e9
Matthew <ikirik59@gmail.com>
2022-06-08 16:26:30 +0300

    Add list length test

--- LinkedList/LinkedList.Tests/DoublyLinkedListTests.cs
+++ LinkedList/LinkedList.Tests/DoublyLinkedListTests.cs
@@ -1,4 +1,6 @@
-﻿namespace LinkedList.Tests;
+﻿using Xunit;
+
+namespace LinkedList.Tests;
 
 public class DoublyLinkedListTests
 {
@@ -9,4 +11,24 @@ public class DoublyLinkedListTests
         _sut = new DoublyLinkedList();
     }
 
+    public static IEnumerable<object[]> LengthTestData()
+    {
+        yield return new object[]{0, Array.Empty<char>() };
+        yield return new object[]{3, new[] {'a', 'b', 'c'} };
+        yield return new object[]{12, "Hello world!".ToArray() };
+    }
+
+    [Theory]
+    [MemberData(nameof(LengthTestData))]
+    public void LengthMatchesElementsAmount(int expected, params char[] elements)
+    {
+        foreach (var element in elements)
+        {
+            _sut.Append(element);
+        }
+
+        var length = _sut.Length();
+        Assert.Equal(expected, length);
+    }
+    
 }
